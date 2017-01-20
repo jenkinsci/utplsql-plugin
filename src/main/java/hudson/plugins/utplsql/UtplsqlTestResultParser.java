2a0ac606a97374b956430c8d4bdccc66a19c5d66
Santiago Toledano <stoledano@autentia.com>
2017-01-20 14:32:00 +0100

    Removed unused imports.

--- src/main/java/hudson/plugins/utplsql/UtplsqlTestResultParser.java
+++ src/main/java/hudson/plugins/utplsql/UtplsqlTestResultParser.java
@@ -38,7 +38,6 @@ import hudson.util.IOUtils;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Serializable;
 import java.io.FileOutputStream;
